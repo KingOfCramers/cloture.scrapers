f4080b5233c14741687f0453bc9527f570da4d25
Harrison Cramer <harrisoncramer@gmail.com>
2020-06-25 16:53:30 -0400

    adding senate committees, sapc

--- server/queue/jobs/senateCommitteeJobs.js
+++ server/queue/jobs/senateCommitteeJobs.js
@@ -24,58 +24,109 @@ const jobs = [
     },
   },
   {
+    type: "puppeteerv1",
     committee: "sasc",
+    collection: "senateCommittee",
+    name: "Senate Armed Services Committee",
+    link: "https://www.armed-services.senate.gov/hearings?c=all",
+    selectors: {
+      layerOne: {
+        depth: 5,
+        rows: "tbody tr.vevent",
+      },
+      layerTwo: {
+        title: "div#main_column h1",
+        date: { label: true, value: "span.date b" },
+        time: { label: true, value: "span.time b" },
+        location: { label: true, value: "span.location b" },
+      },
+    },
+  },
+  {
+    type: "puppeteerv1",
+    committee: "sagc",
+    collection: "senateCommittee",
+    name: "Senate Agriculture Committee",
+    link: "https://www.agriculture.senate.gov/hearings?c=all",
+    selectors: {
+      layerOne: {
+        depth: 5,
+        rows: "tbody tr.vevent",
+      },
+      layerTwo: {
+        title: "h1.main_page_title",
+        date: { label: true, value: "span.date b" },
+        time: { label: true, value: "span.time b" },
+        location: { label: true, value: "span.location b" },
+      },
+    },
+  },
+  {
+    type: "puppeteerv1",
+    committee: "sapc",
+    collection: "senateCommittee",
+    name: "Senate Appropriations Committee",
+    link: "https://www.appropriations.senate.gov/hearings?c=all&mode=list",
+    selectors: {
+      layerOne: {
+        depth: 5,
+        rows: "tbody tr.vevent",
+      },
+      layerTwo: {
+        title: "h1.main_page_title",
+        date: { label: true, value: "span.date b" },
+        time: { label: true, value: "span.time b" },
+        location: { label: true, value: "span.location b" },
+      },
+    },
   },
   //{
-    //committee: "sagc",
+  //committee: "sapc",
   //},
   //{
-    //committee: "sapc",
+  //committee: "sbnk",
   //},
   //{
-    //committee: "sbnk",
+  //committee: "sbdg",
   //},
   //{
-    //committee: "sbdg",
+  //committee: "sstr",
   //},
   //{
-    //committee: "sstr",
+  //committee: "snat",
   //},
   //{
-    //committee: "snat",
+  //committee: "senv",
   //},
   //{
-    //committee: "senv",
+  //committee: "sfin",
   //},
   //{
-    //committee: "sfin",
+  //committee: "shlp",
   //},
   //{
-    //committee: "shlp",
+  //committee: "shsc",
   //},
   //{
-    //committee: "shsc",
+  //committee: "sind",
   //},
   //{
-    //committee: "sind",
+  //committee: "sjud",
   //},
   //{
-    //committee: "sjud",
+  //committee: "srle",
   //},
   //{
-    //committee: "srle",
+  //committee: "seth",
   //},
   //{
-    //committee: "seth",
+  //committee: "ssci",
   //},
   //{
-    //committee: "ssci",
+  //committee: "ssbs",
   //},
   //{
-    //committee: "ssbs",
-  //},
-  //{
-    //committee: "svet",
+  //committee: "svet",
   //},
 ].map((job) => ({ schedule, ...job }));
 
